77a02862faee633d0b85f949237f42b9ad8d6c29
yuanxy92 <yuanxy92@gmail.com>
2024-09-02 14:47:02 +0800

    update

---
+++ pano/pano_tools.py
@@ -0,0 +1,153 @@
+import argparse
+import logging
+import os
+import pickle
+import time
+from dataclasses import dataclass
+
+import open3d as o3d
+import numpy as np
+import cv2
+
+from pano.features import matching
+from pano.bundle_adj import _hom_to_from, traverse, Image, rotation_to_mat, intrinsics
+from pano.stitcher import no_blend, _proj_img_range_border, _add_weights, estimate_resolution
+from pano.stitcher import SphProj, no_blend, linear_blend, multiband_blend, _hat
+
+@dataclass
+class PanoImage:
+    """Patch with all the informations for stitching."""
+    img: np.ndarray
+    # extrinsic matrix [R|t]
+    rot: np.ndarray # R^-1 of the extrinsic matrix
+    trans: np.ndarray # t of the extrinsic matrix
+    intr: np.ndarray
+    pts3d: np.ndarray
+    confidence_mask: np.ndarray
+    distimg: np.ndarray
+    panocenter: np.ndarray = None
+    range: tuple = (np.zeros(2), np.zeros(2))
+
+    def hom(self):
+        """Homography from pixel to normalized coordinates."""
+        return self.rot.T.dot(np.linalg.inv(self.intr))
+    
+    def dist(self):
+        if self.distimg is None:
+            dist_mat = self.pts3d - self.panocenter
+            self.distimg = np.sqrt(np.sum(np.square(dist_mat), axis=2))
+        return self.distimg
+
+    def proj(self):
+        """Return camera projection transform."""
+        return self.intr.dot(self.rot)
+    
+    def cam_center(self):
+        camera_center = - self.rot @ self.trans
+        return camera_center
+    
+def _add_weights_single_channel(img):
+    """Add weights scaled as (x-0.5)*(y-0.5) in normalized coordinates."""
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGRA)
+    height, width = img.shape[:2]
+    img[..., 3] = _hat(height)[:, None] * _hat(width)[None, :]
+    return img
+    
+def compute_center_of_camera_arrays(pano_images):
+    camera_center = 0
+    for pano_image in pano_images:
+        camera_center = camera_center + pano_image.cam_center()
+    camera_center = camera_center / len(pano_images)
+    return camera_center
+
+def convert_dust3r_to_pano(imgs, focals, poses, pts3d, confidence_masks):
+    # init images
+    pano_images = []
+    for idx in range(len(imgs)):
+        rotation_mat = poses[idx][:3, :3]
+        trans_mat = poses[idx][:3, 3]
+        pano_image = PanoImage(img=imgs[idx], rot=rotation_mat.transpose(), trans=trans_mat,
+            intr=intrinsics(focals[idx], (0, 0)), pts3d=pts3d[idx], confidence_mask=confidence_masks[idx], distimg=None)
+        pano_images.append(pano_image)
+    # compute center of all the cameras
+    camera_array_center = compute_center_of_camera_arrays(pano_images)
+    # compute distance to the center of the camera arrays
+    for idx in range(len(pano_images)):
+        pano_images[idx].panocenter = camera_array_center
+        pano_images[idx].dist()
+    return pano_images
+    
+def pano_segment_sky(image):
+    import cv2
+    from scipy import ndimage
+
+    # Convert to HSV
+    if np.issubdtype(image.dtype, np.floating):
+        image = np.uint8(255*image.clip(min=0, max=1))
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Define range for blue color and create mask
+    lower_blue = np.array([0, 0, 100])
+    upper_blue = np.array([30, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue).view(bool)
+
+    # add luminous gray
+    mask |= (hsv[:, :, 1] < 10) & (hsv[:, :, 2] > 150)
+    mask |= (hsv[:, :, 1] < 30) & (hsv[:, :, 2] > 180)
+    mask |= (hsv[:, :, 1] < 50) & (hsv[:, :, 2] > 220)
+
+    # Morphological operations
+    kernel = np.ones((5, 5), np.uint8)
+    mask2 = ndimage.binary_opening(mask, structure=kernel)
+
+    # keep only largest CC
+    _, labels, stats, _ = cv2.connectedComponentsWithStats(mask2.view(np.uint8), connectivity=8)
+    cc_sizes = stats[1:, cv2.CC_STAT_AREA]
+    order = cc_sizes.argsort()[::-1]  # bigger first
+    i = 0
+    selection = []
+    while i < len(order) and cc_sizes[order[i]] > cc_sizes[order[0]] / 2:
+        selection.append(1 + order[i])
+        i += 1
+    mask3 = np.in1d(labels, selection).reshape(labels.shape)
+
+    # mask
+    mask2 = mask2.astype(np.uint8) * 255
+    mask3 = mask3.astype(np.uint8) * 255
+    return mask2, mask3
+
+def project_point_cloud_to_pano(point_cloud, pano_image, intrinsic_matrix):
+    """
+    Projects a point cloud onto a panoramic image.
+
+    Args:
+        point_cloud (o3d.geometry.PointCloud): The point cloud with RGB data.
+        pano_image (np.ndarray): The panoramic image to map the point cloud onto.
+        intrinsic_matrix (np.ndarray): The intrinsic matrix for camera projection.
+
+    Returns:
+        np.ndarray: The panoramic image with mapped point cloud colors.
+    """
+    # Extract points and colors from the point cloud
+    points = np.asarray(point_cloud.points)
+    colors = np.asarray(point_cloud.colors) * 255  # Scale RGB values to 0-255
+
+    # Prepare the output image
+    h, w, _ = pano_image.shape
+    output_image = pano_image.copy()
+
+    # Project points onto the image
+    for i in range(points.shape[0]):
+        point = np.append(points[i], 1)  # Homogeneous coordinates
+        color = colors[i].astype(int)
+
+        # Project 3D point to 2D using intrinsic matrix
+        projected_point = intrinsic_matrix @ point
+        u = int(projected_point[0] / projected_point[2])
+        v = int(projected_point[1] / projected_point[2])
+
+        # Check if the projected point is within the image boundaries
+        if 0 <= u < w and 0 <= v < h:
+            output_image[v, u] = color
+
+    return output_image
--- pano/ptcloud_to_pano.py
+++ pano/ptcloud_to_pano.py
@@ -9,112 +9,9 @@ import open3d as o3d
 import numpy as np
 import cv2
 
-from pano.features import matching
-from pano.bundle_adj import _hom_to_from, traverse, Image, rotation_to_mat, intrinsics
 from pano.stitcher import no_blend, _proj_img_range_border, _add_weights, estimate_resolution
-from pano.stitcher import SphProj, no_blend, linear_blend, multiband_blend, _hat
-
-@dataclass
-class PanoImage:
-    """Patch with all the informations for stitching."""
-    img: np.ndarray
-    # extrinsic matrix [R|t]
-    rot: np.ndarray # R^-1 of the extrinsic matrix
-    trans: np.ndarray # t of the extrinsic matrix
-    intr: np.ndarray
-    pts3d: np.ndarray
-    confidence_mask: np.ndarray
-    distimg: np.ndarray
-    panocenter: np.ndarray = None
-    range: tuple = (np.zeros(2), np.zeros(2))
-
-    def hom(self):
-        """Homography from pixel to normalized coordinates."""
-        return self.rot.T.dot(np.linalg.inv(self.intr))
-    
-    def dist(self):
-        if self.distimg is None:
-            dist_mat = self.pts3d - self.panocenter
-            self.distimg = np.sqrt(np.sum(np.square(dist_mat), axis=2))
-        return self.distimg
-
-    def proj(self):
-        """Return camera projection transform."""
-        return self.intr.dot(self.rot)
-    
-    def cam_center(self):
-        camera_center = - self.rot @ self.trans
-        return camera_center
-    
-def pano_segment_sky(image):
-    import cv2
-    from scipy import ndimage
-
-    # Convert to HSV
-    if np.issubdtype(image.dtype, np.floating):
-        image = np.uint8(255*image.clip(min=0, max=1))
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define range for blue color and create mask
-    lower_blue = np.array([0, 0, 100])
-    upper_blue = np.array([30, 255, 255])
-    mask = cv2.inRange(hsv, lower_blue, upper_blue).view(bool)
-
-    # add luminous gray
-    mask |= (hsv[:, :, 1] < 10) & (hsv[:, :, 2] > 150)
-    mask |= (hsv[:, :, 1] < 30) & (hsv[:, :, 2] > 180)
-    mask |= (hsv[:, :, 1] < 50) & (hsv[:, :, 2] > 220)
-
-    # Morphological operations
-    kernel = np.ones((5, 5), np.uint8)
-    mask2 = ndimage.binary_opening(mask, structure=kernel)
-
-    # keep only largest CC
-    _, labels, stats, _ = cv2.connectedComponentsWithStats(mask2.view(np.uint8), connectivity=8)
-    cc_sizes = stats[1:, cv2.CC_STAT_AREA]
-    order = cc_sizes.argsort()[::-1]  # bigger first
-    i = 0
-    selection = []
-    while i < len(order) and cc_sizes[order[i]] > cc_sizes[order[0]] / 2:
-        selection.append(1 + order[i])
-        i += 1
-    mask3 = np.in1d(labels, selection).reshape(labels.shape)
-
-    # mask
-    mask2 = mask2.astype(np.uint8) * 255
-    mask3 = mask3.astype(np.uint8) * 255
-    return mask2, mask3
-
-def compute_center_of_camera_arrays(pano_images):
-    camera_center = 0
-    for pano_image in pano_images:
-        camera_center = camera_center + pano_image.cam_center()
-    camera_center = camera_center / len(pano_images)
-    return camera_center
-
-def convert_dust3r_to_pano(imgs, focals, poses, pts3d, confidence_masks):
-    # init images
-    pano_images = []
-    for idx in range(len(imgs)):
-        rotation_mat = poses[idx][:3, :3]
-        trans_mat = poses[idx][:3, 3]
-        pano_image = PanoImage(img=imgs[idx], rot=rotation_mat.transpose(), trans=trans_mat,
-            intr=intrinsics(focals[idx], (0, 0)), pts3d=pts3d[idx], confidence_mask=confidence_masks[idx], distimg=None)
-        pano_images.append(pano_image)
-    # compute center of all the cameras
-    camera_array_center = compute_center_of_camera_arrays(pano_images)
-    # compute distance to the center of the camera arrays
-    for idx in range(len(pano_images)):
-        pano_images[idx].panocenter = camera_array_center
-        pano_images[idx].dist()
-    return pano_images
-
-def _add_weights_single_channel(img):
-    """Add weights scaled as (x-0.5)*(y-0.5) in normalized coordinates."""
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGRA)
-    height, width = img.shape[:2]
-    img[..., 3] = _hat(height)[:, None] * _hat(width)[None, :]
-    return img
+from pano.stitcher import SphProj, no_blend, linear_blend, multiband_blend, no_blend
+from pano_tools import PanoImage, _add_weights_single_channel, convert_dust3r_to_pano
 
 def rgbdpano_to_point_cloud(rgb_image, depth_image, conf_image, im_range, resolution):
     """
@@ -146,13 +43,61 @@ def rgbdpano_to_point_cloud(rgb_image, depth_image, conf_image, im_range, resolu
     mask_binary = mask.reshape(-1,).astype(bool)
     points_selected = points[mask_binary]
     colors_selected = colors[mask_binary]
-
     # Create an Open3D point cloud object
     point_cloud = o3d.geometry.PointCloud()
     point_cloud.points = o3d.utility.Vector3dVector(points_selected)
     point_cloud.colors = o3d.utility.Vector3dVector(colors_selected)
-
     return point_cloud
+
+def rgbdpano_to_ptmesh(rgb_image, depth_image, conf_image, im_range, resolution):
+    """
+    Convert RGB and Depth images into a point cloud
+    :param rgb_image: HxWx3 numpy array representing the RGB image
+    :param depth_image: HxW numpy array representing the depth image
+    :param fov_h: Horizontal field of view in radians
+    :param fov_v: Vertical field of view in radians
+    :return: Open3D point cloud object
+    """
+    # Get the image dimensions
+    height, width = depth_image.shape
+    # Calculate the angles for each pixel
+    x_indices, y_indices = np.meshgrid(np.arange(width), np.arange(height))
+    phi = im_range[0][1] + resolution[1] * y_indices + np.pi / 2
+    theta = im_range[0][0] + resolution[0] * x_indices
+    # Calculate the 3D coordinates
+    x = depth_image * np.sin(phi) * np.cos(theta)
+    y = depth_image * np.sin(phi) * np.sin(theta)
+    z = depth_image * np.cos(phi)
+    # Generate mask using threshold
+    th = 0.5
+    mask = np.ones((conf_image.shape[0], conf_image.shape[1]))
+    mask[conf_image < th] = 0
+    triangles = []
+    # Generate vertices
+    vertices = np.stack((x.flatten(), y.flatten(), z.flatten()), axis=1)
+    colors = rgb_image.reshape(-1, 3) / 255.0  # normalize RGB values
+    colors = np.flip(colors, axis=1)
+    mask_binary = mask.reshape(-1,).astype(bool)
+    # Iterate over all the pixels
+    for v in range(height - 1):
+        for u in range(width - 1):
+            idx = v * width + u
+            idx_right = idx + 1
+            idx_down = idx + width
+            idx_down_right = idx + width + 1
+            if mask[v, u] > 0 and mask[v + 1, u] > 0 and mask[v, u + 1] > 0:
+                triangles.append([idx, idx_down, idx_right])
+            if mask[v, u + 1] > 0 and mask[v + 1, u] > 0 and mask[v + 1, u + 1] > 0:
+                triangles.append([idx_right, idx_down, idx_down_right])
+    triangles = np.array(triangles)
+    # Create the mesh object
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(vertices)
+    mesh.triangles = o3d.utility.Vector3iVector(triangles)
+    mesh.vertex_colors = o3d.utility.Vector3dVector(colors)
+    # Optionally, compute vertex normals
+    mesh.compute_vertex_normals()
+    return mesh
 
 def pano_stitch(ba_images, blender=no_blend, equalize=False, crop=False):
     # compute range
@@ -222,18 +167,16 @@ def pano_stitch(ba_images, blender=no_blend, equalize=False, crop=False):
     mosaic_dist = blender(patches_dist, shape)
     mosaic_conf = blender(patches_conf, shape)
     # segment sky
-    mosaic_sky, mosaic_sky_largest_CC = pano_segment_sky(mosaic_rgb)
     # convert RGB-Depth image to point clouds
     ptcloud = rgbdpano_to_point_cloud(mosaic_rgb, mosaic_dist[:, :, 0], mosaic_conf[:, :, 0], im_range, resolution)
+    mesh = rgbdpano_to_ptmesh(mosaic_rgb, mosaic_dist[:, :, 0], mosaic_conf[:, :, 0], im_range, resolution)
 
     # save npy
     with open('./data/pano_9001gate.npy', 'wb') as f:
         np.save(f, mosaic_rgb)
         np.save(f, mosaic_dist)
         np.save(f, mosaic_conf)
-        np.save(f, mosaic_sky)
     # save image
-    cv2.imwrite('./data/pano_9001gate_sky.png', mosaic_sky)
     cv2.imwrite('./data/pano_9001gate.png', mosaic_rgb)
     mosaic_dist_norm = mosaic_dist / np.max(mosaic_dist) * 255
     cv2.imwrite('./data/pano_9001gate_dist.png', mosaic_dist_norm.astype(np.uint8))
@@ -241,6 +184,8 @@ def pano_stitch(ba_images, blender=no_blend, equalize=False, crop=False):
     cv2.imwrite('./data/pano_9001gate_conf.png', mosaic_conf_norm.astype(np.uint8))
     # save point cloud
     o3d.io.write_point_cloud("./data/pano_9001gate.ply", ptcloud)
+    # save mesh
+    o3d.io.write_triangle_mesh("./data/pano_9001gate_mesh.ply", mesh)
 
     return mosaic_rgb, mosaic_dist, mosaic_conf, resolution, im_range
 
@@ -263,38 +208,3 @@ if __name__ == '__main__':
     logging.getLogger('numba').setLevel(logging.WARNING)  # silence Numba
     main()
 
-def project_point_cloud_to_pano(point_cloud, pano_image, intrinsic_matrix):
-    """
-    Projects a point cloud onto a panoramic image.
-
-    Args:
-        point_cloud (o3d.geometry.PointCloud): The point cloud with RGB data.
-        pano_image (np.ndarray): The panoramic image to map the point cloud onto.
-        intrinsic_matrix (np.ndarray): The intrinsic matrix for camera projection.
-
-    Returns:
-        np.ndarray: The panoramic image with mapped point cloud colors.
-    """
-    # Extract points and colors from the point cloud
-    points = np.asarray(point_cloud.points)
-    colors = np.asarray(point_cloud.colors) * 255  # Scale RGB values to 0-255
-
-    # Prepare the output image
-    h, w, _ = pano_image.shape
-    output_image = pano_image.copy()
-
-    # Project points onto the image
-    for i in range(points.shape[0]):
-        point = np.append(points[i], 1)  # Homogeneous coordinates
-        color = colors[i].astype(int)
-
-        # Project 3D point to 2D using intrinsic matrix
-        projected_point = intrinsic_matrix @ point
-        u = int(projected_point[0] / projected_point[2])
-        v = int(projected_point[1] / projected_point[2])
-
-        # Check if the projected point is within the image boundaries
-        if 0 <= u < w and 0 <= v < h:
-            output_image[v, u] = color
-
-    return output_image
