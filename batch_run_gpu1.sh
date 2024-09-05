0fcacf2cbc87a57619a4a9052ba8e8242eb2c994
yuanxy92 <yuanxy92@gmail.com>
2024-09-05 14:33:58 +0000

    merge

--- batch_run_gpu1.sh
+++ batch_run_gpu1.sh
@@ -1,7 +1,3 @@
 export CUDA_DEVICE_ORDER='PCI_BUS_ID'
 export CUDA_VISIBLE_DEVICES='7'
-<<<<<<< HEAD
-python dust3r_pano.py --root /data/xiaoyun/OV6946_Arm_6_cameras/20240829_256px_v2 --dataname data_0829_8 --start 315 --stop 900 --step 1
-=======
-python dust3r_pano.py --root /data/xiaoyun/OV6946_Arm_6_cameras/20240829_256px_v2 --dataname data_0829_8 --start 315 --stop 900 --step 1
->>>>>>> 3a12bdb67efd0394421ac53df0d93b5e3fbbe69f
+python dust3r_pano.py --root /data/xiaoyun/OV6946_Arm_6_cameras/20240829_256px_v2 --dataname data_0829_8 --start 350 --stop 900 --step 1
