6e5d0219f541a39389a18d27874749225d0dc084
Hunter <yuanxy92@gmail.com>
2024-10-01 13:06:52 +0800

    add code for soft gripprt

--- shells/shell_OV6946.sh
+++ shells/shell_OV6946.sh
@@ -4,10 +4,10 @@
 #         --root /home/luvision/project/Code/data/Aurora/Fig_4/Apple/capture_20240929_3 \
 #         --dataname 003
 
-for i in $(seq -w 000 013);
+for i in $(seq -w 000 014);
 do
-   python convert_dust3r_to_3dgs.py \
-        --root /home/luvision/project/Code/data/Aurora/Fig_4/Apple/capture_20240929_4 \
+   python convert_dust3r_to_3dgs_soft_gripper.py \
+        --root /home/luvision/project/Code/data/Aurora/Fig_4/Apple/capture_20240930_2 \
         --dataname $i
 done
 
